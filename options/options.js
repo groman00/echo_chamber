5dea3753ac7b1b9efadd7eb89b411a0c899b18b5
andrewmenendez <andrew@engrade.com>
2016-11-12 08:43:34 -0500

    first swing at options

---
+++ options/options.js
@@ -0,0 +1,21 @@
+// Saves options to chrome.storage.sync.
+function save_options() {
+  var topic = document.getElementById('gt_style').value;
+  // var time= document.getElementById('seconds').value;
+  chrome.storage.sync.set({
+    groupThinkStyle: gt_style
+    // timeSetting:time
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+
+// document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
